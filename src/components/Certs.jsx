995b2b9604478613b2b5a3449efcaf61c185ee32
Daniel Roth <110140817+Dcroth99@users.noreply.github.com>
2024-08-25 15:33:45 -1000

    seo2

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -5,17 +5,17 @@ import CertImage from './CertImage';
 
 export default function Certs() {
   const certs = [
-    { title: 'Django Meta Certification', src: '../images/djangometacert.jpg', alt: 'Waves Social Media' },
-    { title: 'Databases Certification', src: '../images/DatabasesCertificate.jpg', alt: 'Tile Game' },
-    { title: 'Back-end Development Certification', src: '../images/metacert1.jpg', alt: 'TaskTide' },
-    { title: 'Python Programming Certification', src: '../images/pythonmetacert.jpg', alt: 'Portfolio' }
+    { title: 'Django Meta Certification', src: '../images/full-stack-developer-certificate-django-web-framework.jpg', alt: 'Waves Social Media' },
+    { title: 'Databases Certification', src: '../images/full-stack-developer-certificate-database-certificate.jpg', alt: 'Tile Game' },
+    { title: 'Back-end Development Certification', src: '../images/full-stack-developer-certificate-back-end-development.jpg', alt: 'TaskTide' },
+    { title: 'Python Programming Certification', src: '../images/full-stack-developer-certificate-python-programming.jpg', alt: 'Portfolio' }
   ];
 
   return (
     <div className={styles.container}>
       <Navbar />
       <div className={styles.certscontainer}>
-        <h2 className={styles.certstitle}>Certifications</h2>
+        <h2 className={styles.certstitle}>Web-Developer Certifications</h2>
         <ul className={styles.certslist}>
           {certs.map((cert, index) => (
             <li key={index} className={styles.certsitem}>
--- src/components/Landingtext.jsx
+++ src/components/Landingtext.jsx
@@ -7,7 +7,7 @@ export default function Landingtext() {
       <h1 className={styles.h1Landing}>Hi, my name is</h1>
       <h1 className={styles.name}>Daniel Roth</h1>
       <p>
-        I am a <span className={styles.highlight}>Full-Stack Web Developer</span> with experience across multiple programming languages and technologies. I am passionate about creating user-friendly applications that solve real-world problems.
+        I am a <span className={styles.highlight}>Web Developer</span> with experience across multiple programming languages and technologies. I am passionate about creating user-friendly applications that solve real-world problems.
       </p>
       <p className={styles.secondp}>
       I specialize in building robust, scalable web applications using Python, Django, JavaScript, React, and Node.js. My experience includes creating dynamic user interfaces with React and developing powerful APIs and server-side logic with Django.
